405af95047a082683aacdcf734f2046b4fb646f8
sontaya <sontaya@aspiron.co.th>
2022-05-29 19:14:04 +0700

    update today

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,9 @@
 <div>
   <v-data-table
   id="data"
+  disable-sort  
+  :loading="load"
+    loading-text="กำลังโหลดข้อมูล..."
     :headers="headers"
     :items="datata"
     :items-per-page="5"
@@ -14,10 +17,12 @@
    async fetch(){
 this.datata = await this.$axios.$get('https://tongza.000webhostapp.com/')
 this.datata.reverse()
+this.load =false
  console.log('fet api php ',this.datata)
     },
     data () {
       return {datata:[],
+      load:true,
         headers: [    { text: 'เครื่องที่', value: 'engin' },
           { text: 'สถานะ', value: 'status' },
           { text: 'วัน/เวลา', value: 'time' },
