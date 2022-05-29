f39f64d398ad3e133e87d761ea5bcc4e0a22eeaf
sontaya <sontaya@aspiron.co.th>
2022-05-29 11:03:46 +0700

    project

---
+++ layouts/default.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+    
+         <v-btn @click=" exportTableToExcel('data', filename = 'tongza')"    text
+      color="primary"
+    >
+      Export data
+    </v-btn>
+      <v-btn
+        icon
+        @click.stop="clipped = !clipped"
+      >
+ 
+      </v-btn>
+    
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+ 
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'ระบบควบคุมมอเตอร์เหนี่ยวนำ'
+    }
+  },methods: {
+       exportTableToExcel(tableID, filename = ''){
+    var downloadLink;
+    var dataType = 'application/vnd.ms-excel';
+    var tableSelect = document.getElementById(tableID);
+    var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+    // Specify file name
+    filename = filename?filename+'.xls':'excel_data.xls';
+    // Create download link element
+    downloadLink = document.createElement("a");
+    document.body.appendChild(downloadLink);
+    if(navigator.msSaveOrOpenBlob){
+        var blob = new Blob(['\ufeff', tableHTML], {
+            type: dataType
+        });
+        navigator.msSaveOrOpenBlob( blob, filename);
+    }else{
+        // Create a link to the file
+        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+        // Setting the file name
+        downloadLink.download = filename;
+        //triggering the function
+        downloadLink.click();
+    }
+}
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+
+
+
+  
+  <v-data-table
+  id="data"
+    :headers="headers"
+    :items="datata"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
+  </div>
+</template>
+<script>
+  export default {
+   async fetch(){
+this.datata = await this.$axios.$get('https://tongza.000webhostapp.com/')
+console.log('fet api php ',this.datata)
+    },
+    data () {
+      return {datata:[],
+        headers: [
+          { text: 'ลำดับ', value: 'a' },
+          { text: 'เครื่องที่', value: 'n' },
+            { text: 'สถานะ', value: 'p' },
+        ],
+      }
+    },
+    methods: {
+    
+    },
+  }
+</script>
